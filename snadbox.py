eadfc2a54e97109433f05e860403a5a9fdf09b98
gitfoxy1 <ilja.prusakov@gmail.com>
2020-11-08 15:37:46 +0200

    typing + random m and h xy

--- snadbox.py
+++ snadbox.py
@@ -17,16 +17,18 @@ clock = pygame.time.Clock()
 game = Game(heroes=0, monsters=0, map_id=1)
 
 # добавим монстра
-monster1 = Monster.little(name="little_monster_1", xy=[1, 1], game=game)
+m_x = choice([i for i in range(15)])
+m_y = choice([i for i in range(10)])
+monster1 = Monster.little(name="little_monster_1", xy=[m_x, m_y], game=game)
 monster1.start_turn()
 game.monsters.add(monster1)
 game.characters.add(monster1)
 game.map.add_characters([monster1])
 
 #  добавить героя
-x = choice([i for i in range(15)])
-y = choice([i for i in range(10)])
-hero = Hero(name="hero_1", image="hero1.png", xy=[x, y], game=game)
+h_x = choice([i for i in range(15)])
+h_y = choice([i for i in range(10)])
+hero = Hero(name="hero_1", image="hero1.png", xy=[h_x, h_y], game=game)
 game.heroes.add(hero)
 game.characters.add(hero)
 game.map.add_characters([hero])
